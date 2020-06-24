5a8bb6ac0bfb4553a6ec0e0dc36ab8d6402e9286
Arsh <arshdeepgary@gmail.com>
2020-06-23 18:50:08 -0700

    Updates

---
+++ precache-manifest.d9d1367a5f47fef0ad4f4cbf08def3d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f968ed809ae67ed95d7a55698fde32ea",
+    "url": "/react-movie-project/index.html"
+  },
+  {
+    "revision": "fca972e8665cfb4438e3",
+    "url": "/react-movie-project/static/css/main.98f6cc6f.chunk.css"
+  },
+  {
+    "revision": "cd043d17704d1af51b3f",
+    "url": "/react-movie-project/static/js/2.a8f5b448.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-movie-project/static/js/2.a8f5b448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca972e8665cfb4438e3",
+    "url": "/react-movie-project/static/js/main.f721a299.chunk.js"
+  },
+  {
+    "revision": "53342fa6d19f3270db04",
+    "url": "/react-movie-project/static/js/runtime-main.c8564189.js"
+  },
+  {
+    "revision": "08392f186e99a74794065effff6f90e5",
+    "url": "/react-movie-project/static/media/moviedb-logo.08392f18.svg"
+  },
+  {
+    "revision": "d18357f62de90c8e221d393e323a72f6",
+    "url": "/react-movie-project/static/media/not-found.d18357f6.png"
+  },
+  {
+    "revision": "9e7046164b7a4d75f58804c132b64859",
+    "url": "/react-movie-project/static/media/search.9e704616.png"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/react-movie-project/static/media/typescript.3de182d2.svg"
+  }
+]);
